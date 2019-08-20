30944d9a876108c0ad67280f4b0d9eac163ecf55
Ronald Requena <ronaldrequena@Ronalds-MacBook-Pro.local>
2019-08-20 08:29:09 -0500

    initial project

---
+++ src/library/JenkinsLibrary.groovy
@@ -0,0 +1,26 @@
+#!groovy
+package library;
+
+import org.jenkinsci.plugins.docker.workflow.*;
+import com.cloudbees.plugins.credentials.CredentialsProvider;
+import com.cloudbees.plugins.credentials.common.StandardUsernameCredentials;
+import com.cloudbees.plugins.credentials.domains.URIRequirementBuilder;
+import hudson.security.ACL;
+import groovy.json.JsonSlurper;
+import groovy.json.JsonOutput;
+
+class JenkinsLibrary implements Serializable {
+    def steps
+    def scripts
+    def value = "Hola mundo!!!"
+    
+    JenkinsLibrary(steps, script){
+        this.steps = steps
+        this.script = script
+    }
+
+    def showValue(){
+        println value;
+        steps.echo "showValue"
+    }
+}
